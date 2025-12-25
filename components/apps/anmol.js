5ad8af828bc3f9515471b14ba875f7a14dbc577c
Anmol Agarwal <vansh250305@email.com>
2025-12-25 22:56:06 +0530

    refactor: Remove legacy sponsor integration

--- components/apps/anmol.js
+++ components/apps/anmol.js
@@ -384,7 +384,7 @@ function Projects() {
                     <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-full"></div>
                 </div>
             </div>
-            {/* <iframe src="https://github.com/sponsors/AnmollCodes/card" title="Sponsor AnmollCodes" className='my-4 w-5/6 md:w-3/4' ></iframe> */}
+
 
             {
                 project_list.map((project, index) => {
